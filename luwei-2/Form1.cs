37f670038bd0ca87257763f214a8b7b1e648f2e8
Wenxin <wenhsinluo@gmail.com>
2020-02-19 16:17:54 +0800

    按了空白鍵

--- luwei-2/Form1.cs
+++ luwei-2/Form1.cs
@@ -14,7 +14,7 @@ namespace luwei_2
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();  
         }
 
         private void button1_Click(object sender, EventArgs e)
